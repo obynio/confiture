222ed1c990c260c9e6a48c762fbc45bc2a2ee5a3
Yohann LEON <sybiload@gmail.com>
2016-08-04 12:45:44 +0200

    Update aliases.zsh

--- .sybiload/config/zsh/aliases.zsh
+++ .sybiload/config/zsh/aliases.zsh
@@ -1,16 +1,25 @@
 alias ..='cd ..'
 alias ...='cd ../..'
 alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
 alias ls='ls --color=auto'
 alias l='ls -Fh --color=auto'
 alias la='ls -AFh --color=auto'
 alias ll='ls -lFh --color=auto'
 alias lla='ls -lAFh --color=auto'
+alias find='find . -name'
+alias sdisable=' sudo systemctl disable'
+alias senable='sudo systemctl enable'
+alias srestart='sudo systemctl restart'
+alias sstart='sudo systemctl start'
+alias sstatus='sudo systemctl status'
 
 # Fun aliases
-alias fuck='sudo'
 alias rtfm='man'
 alias nyan='telnet nyancat.dakko.us'
+alias fuck="killall -9"
+alias wtfinternet="ping -c 3 192.168.1.1;ping -c 3 www.google.com"
 
 # DIY aliases
 alias nsc='~/.sybiload/bin/nsc'
@@ -18,8 +27,8 @@ alias i3lock='sh ~/.sybiload/shell/lock.sh &> /dev/null'
 alias wallpaper='sh ~/.sybiload/shell/wallpaper.sh &> /dev/null'
 
 # VPN
-alias vpn-on='sudo systemctl start openvpn@client.service'
-alias vpn-off='sudo systemctl stop openvpn@client.service'
+alias vpnon='sudo systemctl start openvpn@client.service'
+alias vpnoff='sudo systemctl stop openvpn@client.service'
 
 # Connman
 alias net='connmanctl'
@@ -30,3 +39,4 @@ alias gac='git add -A && git commit'
 alias bc='bc -ql'
 alias py='python'
 alias pi='python2'
+alias svim='sudo vim'
