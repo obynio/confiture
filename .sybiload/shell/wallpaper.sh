c82b2d61455973ae3241df2862edd3c1d1945841
Yohann LEON <sybiload@gmail.com>
2016-08-04 04:02:31 +0200

    Update wallpaper.sh

--- .sybiload/shell/wallpaper.sh
+++ .sybiload/shell/wallpaper.sh
@@ -6,8 +6,14 @@ if [ -f "$path" ];
 then
 	feh --bg-scale "$path"
 fi	
-while ! ping -q -c 1 -W 1 sybiload.com >/dev/null; do
-	sleep 60
-done
 
-wget -q -O "$path" https://api.sybiload.com/sybiload/wallpaper.php&
+while :
+do
+	$(echo > /dev/tcp/sybiload.com/80) > /dev/null 2>&1
+	result=$?
+	if [[ $result -eq 0  ]]; then
+		wget -q -O "$path" https://api.sybiload.com/sybiload/wallpaper.php
+		break
+	fi
+	sleep 1
+done
