b56503fa82bc8a1cdc7b40e9da61dea41f831b83
Yohann Leon <sybiload@gmail.com>
2017-09-18 17:12:49 +0200

    Update wallpaper.sh

--- .shepard/shell/wallpaper.sh
+++ .shepard/shell/wallpaper.sh
@@ -9,7 +9,7 @@ fi
 
 while :
 do
-	$(echo > /dev/tcp/obyn.io/80) &> /dev/null
+	$(nc -vzw 2 obyn.io 80 > /dev/null 2>&1)
 	result=$?
 	if [[ $result -eq 0  ]]; then
 		wget -q -O "$path" https://api.obyn.io/wallpaper.php?theme=minimalist
